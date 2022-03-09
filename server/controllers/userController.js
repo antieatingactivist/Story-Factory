fd8b4d07103b29cda1bba461635f5a3670275254
samuel lutz <samlutz89@gmail.com>
2022-03-09 14:45:24 -0800

    delete function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,9 +8,6 @@ module.exports = {
   },
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
-    //Specifies which document fields to include or exclude (also known as the query "projection")
-    //When using string syntax, prefixing a path with - will flag that path as excluded. When a path does not have the - prefix, it is included. Lastly, if a path is prefixed with +, it forces inclusion of the path, which is useful for paths excluded at the schema level.
-    // https://mongoosejs.com/docs/api.html#query_Query-select
       .select('-__v')
       .populate('posts')
       .then((user) =>
@@ -24,6 +21,16 @@ module.exports = {
   createUser(req, res) {
     User.create(req.body)
       .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Application.deleteMany({ _id: { $in: user.applications } })
+      )
+      .then(() => res.json({ message: 'User and associated apps deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
 };
--- server/routes/api/userRoutes.js
+++ server/routes/api/userRoutes.js
@@ -5,13 +5,13 @@ const {
     getSingleUser, 
     updateUser, 
     deleteUser, 
-    addFriend, 
-    deleteFriend 
+    // addFriend, 
+    // deleteFriend 
 } = require('../../controllers/userController');
 
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
+// router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
