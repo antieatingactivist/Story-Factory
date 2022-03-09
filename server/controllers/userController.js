f37897b3a694e91d7bdf837b0ea78bae638200af
samuel lutz <samlutz89@gmail.com>
2022-03-09 14:49:36 -0800

    adding get all users and update user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -33,4 +33,44 @@ module.exports = {
       .then(() => res.json({ message: 'User and associated apps deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
+  updateUser({params, body}, res) {
+    User.findOneAndUpdate({
+            _id: params.id
+        }, body, {
+            new: true,
+            runValidators: true
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({
+                    message: 'No user found with this id.'
+                });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+  },
+  getAllUsers(req, res) {
+    User.find({})
+        .populate({
+            path: 'thoughts',
+            //allows to remove __v from visuals
+            select: ('-__v')
+        })
+        .populate({
+            path: 'friends',
+            select: ('-__v')
+        })
+        .select('-__v')
+        // sort by descending order by the _id value
+        .sort({
+            _id: -1
+        })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+  },
 };
