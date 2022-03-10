b619282552ee0f50ad2b13f44687f85a9a94dcfb
antieatingactivist <antieatingactivist@gmail.com>
2022-03-09 20:45:38 -0800

    created nav

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,29 @@
 
-
+import Nav from './Nav';
 
 
 export default function Header({hidden, scrollPosition}) {
 
     const headerStyle = {
-        paddingTop: "10px"
+        paddingTop: "10px",
+        display: "flex",
+        width: "100%",
+        fontFamily: "American Typewriter",
     }
     const titleStyle = {
-        fontFamily: "American Typewriter",
+        
         fontSize: "4em",
-        paddingLeft: "30px"
+        paddingLeft: "30px",
+        // backgroundColor: "#000000",
+        whiteSpace: "nowrap"
+        
     }
 
 
     return (
         <header style={headerStyle}>
-            <span style={titleStyle}>Story Factory</span>
-
+            <div style={titleStyle}>Story Factory</div>
+            <Nav />
       
         </header>
     );
