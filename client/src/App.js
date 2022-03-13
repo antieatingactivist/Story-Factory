f38e641e85f569f1d93e76bcc2264c0d9d2f10bd
samuel lutz <samlutz89@gmail.com>
2022-03-13 13:46:08 -0700

    fixed spelling in app.js for story

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ function App() {
         <Header />
     
         <Create />
-        {storyShow.homeShow ? <Home /> : <Story />}
+        {Story.homeShow ? <Home /> : <Story />}
      
         {globalState.homeShow ? <Home /> : <Story />}
       
