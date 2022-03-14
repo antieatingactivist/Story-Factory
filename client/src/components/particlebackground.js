b8e987a15d15cf40d31474b2adff188d61efbf48
samuel lutz <samlutz89@gmail.com>
2022-03-14 08:02:48 -0700

    particles flickering

--- client/src/components/particlebackground.js
+++ client/src/components/particlebackground.js
@@ -25,8 +25,31 @@ const particleBackground =() =>{
                         area: 1200
                         },
                         limit: 0,
-                        value: 400,
+                        value: 200,
                     },
+                    opacity:{
+                        animation:{
+                            enable: true,
+                            minimumValue: 0.05,
+                            speed: 1,
+                            sync: false
+                        },
+                        random: {
+                            enable: true,
+                            minimumValue: 0.05,
+                        },
+                        value: 1
+                    },
+                    shape:{
+                        type: 'circle'
+                    },
+                    size:{
+                        random: {
+                            enable: true,
+                            minimumValue: 0.5
+                        },
+                        value: 1,
+                    }
                 }
             }}/>
         </div>
