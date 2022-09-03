82bedbe5fe0fe938c783178440791a043786a404
antieatingactivist <antieatingactivist@gmail.com>
2022-09-02 17:08:35 -0700

    updated for https

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+require('dotenv').config();
 const app = express();
+const https = require('https');
+const fs = require('fs');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,5 +19,13 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+  // app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+  https.createServer({
+    key: fs.readFileSync(process.env.KEY_PEM),
+    cert: fs.readFileSync(process.env.CERT_PEM),
+  },app)
+    .listen(PORT, ()=>{
+      console.log(`App listening on PORT ${PORT}`);
+      console.log(process.env);
+    });
 });
